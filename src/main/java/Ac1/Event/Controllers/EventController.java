e3418f7e8de25b77f195eb6586fb8978e0a62ef5
Decowo <joeljunior933@gmail.com>
2021-03-27 19:45:28 -0300

    Subindo prod

--- src/main/java/Ac1/Event/Controllers/EventController.java
+++ src/main/java/Ac1/Event/Controllers/EventController.java
@@ -11,9 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/Events")
